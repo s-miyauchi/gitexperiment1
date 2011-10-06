6b8b8da0cd959d92ab1b28495c1eebea35ba80b0
shun <nickne186@gmail.com>
2011-10-06 22:16:47 +0900

    First Commit Version 1

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gitexperiment1
+{
+    public partial class Form1 : Form
+    {
+        private int x;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            x = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x++;
+            this.label1.Text = x.ToString();
+        }
+    }
+}
